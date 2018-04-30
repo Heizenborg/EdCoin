9130845ee636f164a4d30351e26b714a3c99052f
ScottGetchel <scott.getchel@gmail.com>
2018-04-30 13:27:01 -0700

    added wallet model

---
+++ Classes/Wallet.py
@@ -0,0 +1,11 @@
+import hashlib as hasher
+
+
+class Wallet:
+	def __init__(self, tuition, coins):
+		self.owner = owner
+		self.tuition = tuition
+		self.coins = coins
+
+	def (self, tuition, transactioin):
+		return 
